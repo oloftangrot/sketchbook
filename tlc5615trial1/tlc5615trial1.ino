5eb9221d40a0f29b7c72d1aa8f4258f2767eee1c
olof <olof@localhost.localdomain>
2015-01-17 08:47:58 +0100

    Fixed tlc5615, verified on Uno

--- tlc5615trial1/tlc5615trial1.ino
+++ tlc5615trial1/tlc5615trial1.ino
@@ -1,71 +1,38 @@
 /*
-  Digital Pot Control
+  DAC using TLC5615
   
-  This example controls an Analog Devices AD5206 digital potentiometer.
-  The AD5206 has 6 potentiometer channels. Each channel's pins are labeled
-  A - connect this to voltage
-  W - this is the pot's wiper, which changes when you set it
-  B - connect this to ground.
- 
- The AD5206 is SPI-compatible,and to command it, you send two bytes, 
- one with the channel number (0 - 5) and one with the resistance value for the
- channel (0 - 255).  
- 
  The circuit:
-  * All A pins  of AD5206 connected to +5V
-  * All B pins of AD5206 connected to ground
-  * An LED and a 220-ohm resisor in series connected from each W pin to ground
-  * CS - to digital pin 10  (SS pin)
-  * SDI - to digital pin 11 (MOSI pin)
-  * CLK - to digital pin 13 (SCK pin)
+  * DIP 8  of TLC5615 connected to +5V
+  * DIP 5 of TLC5615 connected to ground
+  * DIP 3 CS of TLC5615 connected digital pin 10  (SS pin)
+  * DIP 1 DIN of TLC5615 connected ISP 4 (MOSI pin)
+  * DIP 2 SCLK of TLC5615 connectedPin ISP 2 (SCK pin)
  
- created 10 Aug 2010 
- by Tom Igoe
- 
- Thanks to Heather Dewey-Hagborg for the original tutorial, 2005
+ created 17 Jan 2015 
+ by Olof Tångrot
  
 */
+#include <SPI.h> // include the SPI library:
+const int slaveSelectPin = 10; // set pin 10 as the slave select for the digital pot:
 
-
-// inslude the SPI library:
-#include <SPI.h>
-
-
-// set pin 10 as the slave select for the digital pot:
-const int slaveSelectPin = 10;
+unsigned int i;
 
 void setup() {
-  // set the slaveSelectPin as an output:
-  pinMode (slaveSelectPin, OUTPUT);
-  // initialize SPI:
-  SPI.begin(); 
+  pinMode (slaveSelectPin, OUTPUT);   // set the slaveSelectPin as an output:
+  SPI.begin();   // initialize SPI:
+  SPI.setBitOrder( MSBFIRST ) ;
 }
 
-void loop() {
-  // go through the six channels of the digital pot:
-  for (int channel = 0; channel < 6; channel++) { 
-    // change the resistance on this channel from min to max:
-    for (int level = 0; level < 255; level++) {
-      digitalPotWrite(channel, level);
-      delay(10);
-    }
-    // wait a second at the top:
-    delay(100);
-    // change the resistance on this channel from max to min:
-    for (int level = 0; level < 255; level++) {
-      digitalPotWrite(channel, 255 - level);
-      delay(10);
-    }
-  }
-
+void loop() { 
+  tlc5615Write( i );
+  i++;
+  if ( i >= 1024 ) i = 0; // /|/|/|/|/|/|/|/|
 }
 
-void digitalPotWrite(int address, int value) {
-  // take the SS pin low to select the chip:
-  digitalWrite(slaveSelectPin,LOW);
-  //  send in the address and value via SPI:
-  SPI.transfer(address);
-  SPI.transfer(value);
-  // take the SS pin high to de-select the chip:
-  digitalWrite(slaveSelectPin,HIGH); 
+void tlc5615Write( unsigned int d ) {
+  d = d << 2;   /* Condition the data, two LSB 0's, four MSB dummy bits */
+  digitalWrite(slaveSelectPin,LOW);   // take the SS pin low to select the chip:
+  SPI.transfer( d >> 8 );
+  SPI.transfer( d );
+  digitalWrite(slaveSelectPin,HIGH);   // take the SS pin high to de-select the chip:
 }
