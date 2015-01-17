4695d7893bd05f487cd7a64d73947a53f3d5e902
olof <olof@localhost.localdomain>
2015-01-17 19:44:54 +0100

    Played with tlc5615 trial

--- tlc5615trial1/tlc5615trial1.ino
+++ tlc5615trial1/tlc5615trial1.ino
@@ -14,19 +14,53 @@
 */
 #include <SPI.h> // include the SPI library:
 const int slaveSelectPin = 10; // set pin 10 as the slave select for the digital pot:
+const int sig1_table_size = 128; 
+const int sig2_table_size = 100; 
 
 unsigned int i;
+unsigned int time;
+unsigned int sig1[sig1_table_size];
+unsigned int sig2[sig2_table_size];
+boolean sig = false;
+unsigned int siz = sig1_table_size;
+unsigned int* tab = sig1;
 
 void setup() {
   pinMode (slaveSelectPin, OUTPUT);   // set the slaveSelectPin as an output:
   SPI.begin();   // initialize SPI:
   SPI.setBitOrder( MSBFIRST ) ;
+  SPI.setClockDivider( SPI_CLOCK_DIV2 ); // Set clock to 8 MHz.
+  for ( int j = 0; j < sig1_table_size; j++ ) {
+    sig1[j] = (unsigned int) ( 512 + 511 * sin ( j * 2 * 3.1415f/sig1_table_size ) );
+  }
+  for ( int j = 0; j < sig2_table_size; j++ ) {
+    sig2[j] = (unsigned int) ( 512 + 511 * sin ( j * 2 * 3.1415f/sig2_table_size ) );
+  }
 }
 
 void loop() { 
+#if 0  
   tlc5615Write( i );
   i++;
-  if ( i >= 1024 ) i = 0; // /|/|/|/|/|/|/|/|
+  if ( i >= 1024 ) i = 0; // ~~~
+#endif
+#if 1
+  tlc5615Write( tab[i++] );
+  if ( i >= siz )  i = 0; //  
+  if ( time++ > 30000 ) {
+    if ( sig ) {
+      tab = sig2;
+      siz = sig2_table_size;
+    }
+    else {
+      tab = sig1;
+      siz = sig1_table_size;
+    }
+    i = 0; // Just reset the table index
+    sig = !sig;
+    time = 0;
+  }
+#endif 
 }
 
 void tlc5615Write( unsigned int d ) {
