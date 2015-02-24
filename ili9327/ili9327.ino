eee43483e6ba1fb37520661d525d9e507bb2e212
Olof <olof@localhost.localdomain>
2015-02-24 22:27:31 +0100

    Fixed 400x240 display

--- ili9327/ili9327.ino
+++ ili9327/ili9327.ino
@@ -5,70 +5,69 @@
 #define LCD_CS   A3       
 #define LCD_REST A4
 
-void Lcd_Writ_Bus(char VH)
+void Lcd_Writ_Bus( char VH )
 {
   unsigned char i,temp,data; 
-  data=VH;
-  for(i=8;i<=9;i++)
+  data = VH;
+  for ( i = 8; i <= 9; i++ )
   {
-    temp=(data&0x01);
+    temp = ( data & 0x01 );
     if(temp)
-      digitalWrite(i,HIGH);
+      digitalWrite( i, HIGH );
     else
-      digitalWrite(i,LOW);
-    data=data>>1;
+      digitalWrite( i, LOW );
+    data = data >> 1;
   }	
-  for(i=2;i<=7;i++)
+  for ( i = 2; i <= 7; i++ )
   {
-    temp=(data&0x01);
-    if(temp)
-      digitalWrite(i,HIGH);
+    temp = ( data & 0x01 );
+    if( temp )
+      digitalWrite( i, HIGH );
     else
-      digitalWrite(i,LOW);
-    data=data>>1;
+      digitalWrite( i, LOW );
+    data = data >> 1;
   }	 
 
-  digitalWrite(LCD_WR,LOW);
-  digitalWrite(LCD_WR,HIGH);
+  digitalWrite( LCD_WR, LOW );
+  digitalWrite( LCD_WR, HIGH );
 }
 
-
-void Lcd_Write_Com(char VH)  
+void Lcd_Write_Com( char VH )  
 {   
-  digitalWrite(LCD_RS,LOW);
-  Lcd_Writ_Bus(VH);
+  digitalWrite( LCD_RS, LOW );
+  Lcd_Writ_Bus( VH );
 }
 
-void Lcd_Write_Data(char VH)
+void Lcd_Write_Data( char VH )
 {
-  digitalWrite(LCD_RS,HIGH);
-  Lcd_Writ_Bus(VH);
+  digitalWrite( LCD_RS, HIGH );
+  Lcd_Writ_Bus( VH );
 }
 
-void Lcd_Write_Com_Data(int com,int dat)
+void Lcd_Write_Com_Data( int com, int dat )
 {
-  Lcd_Write_Com(com);
-  Lcd_Write_Data(dat);
+  Lcd_Write_Com( com );
+  Lcd_Write_Data( dat );
 }
 
-void Address_set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2)
+void Address_set( unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2 )
 {
-  Lcd_Write_Com(0x2a); // Set_column_address 4 parameters
-  Lcd_Write_Data(x1>>8);
-  Lcd_Write_Data(x1);
-  Lcd_Write_Data(x2>>8);
-  Lcd_Write_Data(x2);
+  Lcd_Write_Com( 0x2a ); // Set_column_address 4 parameters
+  Lcd_Write_Data( x1 >> 8 );
+  Lcd_Write_Data( x1 );
+  Lcd_Write_Data( x2 >> 8 );
+  Lcd_Write_Data( x2 );
 
-  Lcd_Write_Com(0x2b); // Set_page_address 4 parameters
-  Lcd_Write_Data(y1>>8);
-  Lcd_Write_Data(y1);
-  Lcd_Write_Data(y2>>8);
-  Lcd_Write_Data(y2);
+  Lcd_Write_Com( 0x2b ); // Set_page_address 4 parameters
+  Lcd_Write_Data( y1 >>8 );
+  Lcd_Write_Data( y1 );
+  Lcd_Write_Data( y2 >> 8 );
+  Lcd_Write_Data( y2 );
 
-  Lcd_Write_Com(0x2c); // Write_memory_start								 
+  Lcd_Write_Com( 0x2c ); // Write_memory_start								 
 }
 
-void Lcd_Init(void)
+void Lcd_Init( void )
 {
   digitalWrite(LCD_REST,HIGH);
   delay(5); 
@@ -158,19 +157,19 @@ void Lcd_Init(void)
 
   Lcd_Write_Com(0x2C); //display on 
 
-  digitalWrite(LCD_CS,HIGH);
+  digitalWrite( LCD_CS, HIGH );
 }
 
 void H_line(unsigned int x, unsigned int y, unsigned int l, unsigned int c)                   
 {	
-  unsigned int i,j;
-  Lcd_Write_Com(0x02c); //write_memory_start
-  digitalWrite(LCD_RS,HIGH);
-  digitalWrite(LCD_CS,LOW);
-  l=l+x;
-  Address_set(x,y,l,y);
-  j=l*2;
-  for(i=1;i<=j;i++)
+  unsigned int i, j;
+  Lcd_Write_Com( 0x02c ); //write_memory_start
+  digitalWrite( LCD_RS,HIGH );
+  digitalWrite( LCD_CS,LOW );
+  l = l + x;
+  Address_set(x, y, l, y );
+  j = l * 2;
+  for ( i = 1; i <= j; i++ )
   {
     Lcd_Write_Data(c);
   }
@@ -180,13 +179,13 @@ void H_line(unsigned int x, unsigned int y, unsigned int l, unsigned int c)
 void V_line(unsigned int x, unsigned int y, unsigned int l, unsigned int c)                   
 {	
   unsigned int i,j;
-  Lcd_Write_Com(0x02c); //write_memory_start
-  digitalWrite(LCD_RS,HIGH);
-  digitalWrite(LCD_CS,LOW);
+  Lcd_Write_Com( 0x02c ); //write_memory_start
+  digitalWrite( LCD_RS, HIGH );
+  digitalWrite( LCD_CS, LOW );
   l=l+y;
-  Address_set(x,y,x,l);
-  j=l*2;
-  for(i=1;i<=j;i++)
+  Address_set( x, y, x, l );
+  j = l * 2;
+  for ( i = 1; i <= j; i++ )
   {
     Lcd_Write_Data(c);
   }
@@ -204,62 +203,63 @@ void Rect(unsigned int x,unsigned int y,unsigned int w,unsigned int h,unsigned i
 void Rectf(unsigned int x,unsigned int y,unsigned int w,unsigned int h,unsigned int c)
 {
   unsigned int i;
-  for(i=0;i<h;i++)
+  for ( i = 0; i < h; i++ )
   {
-    H_line(x  , y  , w, c);
-    H_line(x  , y+i, w, c);
+    H_line( x, y  , w, c);
+    H_line( x, y+i, w, c);
   }
 }
 
-void LCD_Clear(unsigned int j)                   
+void LCD_Clear( unsigned int j )                   
 {	
-  unsigned int i,m;
-  Lcd_Write_Com(0x02c); //write_memory_start
-  digitalWrite(LCD_RS,HIGH);
-  digitalWrite(LCD_CS,LOW);
-  Address_set(0,0,479,399);
+  unsigned int i, m;
+  Lcd_Write_Com( 0x02c ); //write_memory_start
+  digitalWrite( LCD_RS, HIGH );
+  digitalWrite( LCD_CS, LOW );
+  Address_set( 0, 0, 479, 399 );
 
-  for(i=0;i<480;i++)
-    for(m=0;m<400;m++)
+  for ( i = 0; i < 480; i++ )
+    for ( m = 0; m < 400; m++ )
     {
-      Lcd_Write_Data(j);
+      Lcd_Write_Data( j );
     }
   digitalWrite(LCD_CS,HIGH);   
 }
 
 void setup()
 {
-  for(int p=2;p<10;p++)
+  for(int p = 2; p < 10; p++ )
   {
-    pinMode(p,OUTPUT);
+    pinMode( p, OUTPUT );
   }
-  pinMode(A0,OUTPUT);
-  pinMode(A1,OUTPUT);
-  pinMode(A2,OUTPUT);
-  pinMode(A3,OUTPUT);
-  pinMode(A4,OUTPUT);
-  digitalWrite(A0, HIGH);
-  digitalWrite(A1, HIGH);
-  digitalWrite(A2, HIGH);
-  digitalWrite(A3, HIGH);
-  digitalWrite(A4, HIGH);
+  pinMode( A0, OUTPUT );
+  pinMode( A1, OUTPUT );
+  pinMode( A2, OUTPUT );
+  pinMode( A3, OUTPUT );
+  pinMode( A4, OUTPUT );
+  digitalWrite( A0, HIGH );
+  digitalWrite( A1, HIGH );
+  digitalWrite( A2, HIGH );
+  digitalWrite( A3, HIGH );
+  digitalWrite( A4, HIGH );
   Lcd_Init();
   LCD_Clear(0x00);
 }
 
 void loop()
 {  
-/*
-  for(int i=0;i < 10;i++)
-  {
-    Rect(random(300),random(300),random(300),random(300),random(65535)); // rectangle at x, y, with, hight, color
+
+  for ( int i = 0; i < 200; i++ )
+  { // rectangle at x, y, with, hight, color
+    Rect( random(120), random(200), random(120), random(200), random(65535)); 
   }
   LCD_Clear(0x00);
-*/
+/*
   for ( int color=1; color<65535; color++) {
     for( int x = 290; x< 300; x++ ) {
       H_line( 200, x, 50, color );
     }
   }
+  */
 }
 
