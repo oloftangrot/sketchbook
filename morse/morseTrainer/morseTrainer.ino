5cfd45f421de7c428f62928eddd2a895723cfc77
Olof <olof@localhost.localdomain>
2017-12-24 22:58:05 +0100

    Possibly fixed fault in queue.

--- morse/morseTrainer/morseTrainer.ino
+++ morse/morseTrainer/morseTrainer.ino
@@ -2,42 +2,37 @@
   morseTrainer
  */
 
-// digital pin 2 has a pushbutton attached to it. Give it a name:
-const unsigned char key = 2;
-const unsigned char key_out = 5;
-const unsigned char host_out = 6;
-const unsigned char ON_DUTYCYCLE = 100;
-int count = 0;
-int xoff_cnt = 0;
-int rise_cnt = 0;
-
-void test(void);
-
 #define MAXEVENT (100)
-struct {
-  boolean b;
-  int time ;
-} queue[MAXEVENT];
-
-unsigned char q_in=0;
-unsigned char q_out=0;
-unsigned char q_size = 0;
+const unsigned char KEY = 2; // digital pin 2 has a pushbutton attached to it. Give it a name:
+const unsigned char KEY_OUT = 5;
+const unsigned char HOST_OUT = 6;
+const unsigned char ON_DUTYCYCLE = 100;
 const unsigned char XON_SIZE = 20;
 const unsigned char XOFF_SIZE = MAXEVENT - XON_SIZE;
 const char XOFF = 19;
 const char XON = 17;
 const char LF = 10; // Ctrl-J
+int count = 0;
+int xoff_cnt = 0;
+int rise_cnt = 0;
+unsigned char q_in=0;
+unsigned char q_out=0;
+unsigned char q_size = 0;
 
+void test(void);
 void pullQueue(void);
+
+struct {
+  boolean b;
+  int time ;
+} queue[MAXEVENT];
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(115200);
-  pinMode(key, INPUT);
-  pinMode(key_out, OUTPUT);
-  pinMode(host_out, OUTPUT);
-  analogWrite( host_out, 0);
-  analogWrite( key_out, 0);
+  pinMode(KEY, INPUT);
+  pinMode(KEY_OUT, OUTPUT);
+  pinMode(HOST_OUT, OUTPUT);
   Serial.write( ">");
 }
 
@@ -60,11 +55,11 @@ boolean rx = false;
 void loop() {
 //  test();
   // read the input pin:
-  if ( digitalRead( key ) ) {
-    analogWrite( key_out, ON_DUTYCYCLE );
+  if ( digitalRead( KEY ) ) {
+    analogWrite( KEY_OUT, ON_DUTYCYCLE );
   }
   else {
-    analogWrite( key_out, 0 );
+    analogWrite( KEY_OUT, 0 );
   }
   
   // print out the state of the button:
@@ -79,9 +74,9 @@ void pullQueue( void )
   if ( q_size ) {
     out_state = queue[q_out].b;
     if ( out_state )
-      analogWrite( host_out, ON_DUTYCYCLE );
+      analogWrite( HOST_OUT, ON_DUTYCYCLE );
     else
-      analogWrite( host_out, 0 );
+      analogWrite( HOST_OUT, 0 );
     t_change = millis() - t0; // Store set time.
     Serial.write('#');
     Serial.println( t_change );
@@ -91,7 +86,7 @@ void pullQueue( void )
     
     q_out++;
     q_size--;
-    if ( q_out >= MAXEVENT ) q_out = 0; // Wrap pointer. 
+    if ( q_out >= ( MAXEVENT - 1 ) ) q_out = 0; // Wrap pointer. 
   }
   else { // Queue is empty.
     t_next = millis();
@@ -129,14 +124,14 @@ void serialEvent() {
     rx = false;
     queue[q_in++].time = t;
     q_size++;
-    if ( q_in >= MAXEVENT ) q_in = 0;
+    if ( q_in >= ( MAXEVENT - 1) ) q_in = 0;
     if ( XOFF_SIZE == q_size ) {
       Serial.write( XOFF );
       xoff_cnt++;
     }
 //  Serial.write('E');
   }
-  else if ('?' == c ) {
+  else if ( '?' == c ) {
     Serial.println( count );
     Serial.println( xoff_cnt );
     Serial.println( rise_cnt );
@@ -146,9 +141,9 @@ void serialEvent() {
 
 void test(void) {
   for(;;) {
-    digitalWrite(key_out,true);  
+    digitalWrite( KEY_OUT, true );  
     delay(1);
-    digitalWrite(key_out,false);
+    digitalWrite( KEY_OUT, false );
     delay(1);  
   }
 }
